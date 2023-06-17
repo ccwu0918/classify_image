40dc1953fdb34b3efcb68cc8059b62000fe6327a
Chia-Chun Wu <ccwu0918@gmail.com>
2023-06-18 00:16:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -23,24 +23,21 @@ num_classes = len(labels)
 base_dir = './classify_image/'
 
 # 載入並檢視訓練完成的模型。
-
 model = load_model('my_cnn_model.h5') # Loading the Tensorflow Saved Model (PB)
 print(model.summary())
 
 # 注意現在主函數做辨識只有五個種類。而且是使用我們自行訓練的 model!
-
 def classify_image(inp):
     inp = inp.reshape((-1, 256, 256, 3))
     inp = preprocess_input(inp)
     prediction = model.predict(inp).flatten()
-    return {labels[i]: float(prediction[i]) for i in range(5)}
+    return {labels[i]: float(prediction[i]) for i in range(num_classes)}
 
 image = gr.Image(shape=(256, 256), label="栗喉蜂虎、藍孔雀、戴勝、鱟及歐亞水獺照片")
 label = gr.Label(num_top_classes=num_classes, label="AI ResNet50V2遷移式學習辨識結果")
 some_text="我能辨識金門栗喉蜂虎、藍孔雀、戴勝、鱟及歐亞水獺。找張金門栗喉蜂虎、藍孔雀、戴勝、鱟及歐亞水獺照片來考我吧!"
 
 # 我們將金門栗喉蜂虎、藍孔雀、戴勝、鱟及歐亞水獺數據庫中的圖片拿出來當作範例圖片讓使用者使用
-
 sample_images = []
 for i in range(num_classes):
     thedir = base_dir + image_folders[i]
@@ -50,7 +47,6 @@ for i in range(num_classes):
         sample_images.append(base_dir + image_folders[i] + '/' + file)
 
 # 最後，將所有東西組裝在一起，就大功告成了！
-        
 iface = gr.Interface(fn=classify_image,
              inputs=image,
              outputs=label,
